a1a8d14baefa3b22dcce1a38c47958cb4af047ea
AxAnder0095 <doriscafe@hotmail.com>
2026-02-07 15:56:46 -0600

    debugging fix

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import weatherRoutes from './routes/weatherRoutes.js';
@@ -7,6 +8,15 @@ dotenv.config({ path: path.join(process.cwd(), 'server', '.env') });
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(
+    cors({
+        origin: [
+            'http://localhost:5173',
+            'https://weather-application-indol-tau.vercel.app'
+        ]
+    })
+);
 
 app.use('/api', weatherRoutes);
 
